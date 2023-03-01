f0ad094781b37f439e5095c6f872d9e48f68dbed
Tom van Liempd <tomvanliempd@hotmail.nl>
2023-03-01 17:24:49 +0100

    Added ability to specify webcam resolution

--- cardrobot/main.py
+++ cardrobot/main.py
@@ -3,18 +3,18 @@ load_dotenv()
 
 from ultralytics.yolo.v8 import detect
 import os
+from webcam import WebcamSource
 
 def predict():
     predictor = detect.DetectionPredictor(overrides={
         'show': True,
         'save': False,
         'verbose': False,
-        'source': os.getenv('WEBCAM_SOURCE'),
         'model': os.path.abspath(os.path.join(os.path.realpath(__file__), '../../model/best_weights.pt'))
     })
 
     # The predictor function will keep predicting and returning values as we go
-    for data in predictor(stream=True):
+    for data in predictor(WebcamSource(os.getenv('WEBCAM_SOURCE'), os.getenv('WEBCAM_RESOLUTION'), predictor), stream=True):
         # data contains all predictions made in the current frame/image from the webcam
 
         data = data.to("cpu") # Move data to CPU
