6e2b44aa3390003aa862ac5f54f19ebdc83b3a62
Tom van Liempd <tomvanliempd@hotmail.nl>
2023-03-15 22:17:33 +0100

    Updated model

--- model/upload_model.py
+++ model/upload_model.py
@@ -12,6 +12,6 @@ rf = Roboflow(api_key=os.getenv("ROBOFLOW_API_KEY"))
 project = rf.workspace(os.getenv("ROBOFLOW_WORKSPACE")).project(os.getenv("ROBOFLOW_PROJECT"))
 project.version(int(os.getenv("ROBOFLOW_DATASET_VERSION"))).deploy(
     model_type="yolov8",
-    model_path=train_path
+    model_path=train_path + "/"
 )
 
